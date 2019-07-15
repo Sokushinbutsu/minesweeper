b1bf57ccdf0272f6453c9bc27e5bf7ceed55b1b3
Brian Nelson <brian.allen.nilsson@gmail.com>
2019-07-14 21:32:25 -0400

    buttons render as grid

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import Square from './Square.jsx';
 
 const Board = props => {
-  console.log(props);
-  return <p />;
+  return (
+    <div className="board">
+      {props.board.map((arr, index) => {
+        return arr.map(square => {
+          return <Square square={square} />;
+        });
+      })}
+    </div>
+  );
 };
 
 export default Board;
